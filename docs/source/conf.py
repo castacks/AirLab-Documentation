bea956a8560211091c454e05a0ab50b907aa98bf
Andrew Jong <andrewjong87@gmail.com>
2021-05-14 20:00:58 -0400

    Change sidebar header to AirLab red

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,6 +50,10 @@ exclude_patterns = []
 #
 html_theme = "sphinx_rtd_theme"
 
+html_css_files = [
+    'css/custom.css',
+]
+
 html_theme_options = {
     'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
     'analytics_anonymize_ip': False,
@@ -58,7 +62,7 @@ html_theme_options = {
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
+    'style_nav_header_background': '#cc002b',
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
